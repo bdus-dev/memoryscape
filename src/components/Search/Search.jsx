8b540dc5c41f1c3fcbf66326fe5a1fdd95d40671
bdus-dev <developer@bradypus.net>
2019-11-22 16:04:59 +0100

    Serach uses InternalTmpl

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { FormattedHTMLMessage } from 'react-intl';
 import { makeStyles } from '@material-ui/styles';
-import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 
-import Bar from '../Bar/Bar';
 import backGroundImage from '../../img/internal-background.jpg';
+
+import InternalTmpl from '../Templates/InternalTmpl';
 
 import qs from 'qs';
 
@@ -17,15 +17,6 @@ import Results from './Results';
 import FilterModal from './FilterModal';
 
 const useStyles = makeStyles((theme) => ({
-  homeContainer: {
-    backgroundImage: `url(${backGroundImage})`,
-    backgroundPosition: 'center center',
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover',
-    maxWidth: '100%',
-    backgroundAttachment: 'fixed',
-    minHeight: '100vh',
-  },
   boxContainer: {
     padding: theme.spacing(3, 2),
     height: 200,
@@ -45,12 +36,7 @@ const useStyles = makeStyles((theme) => ({
     letterSpacing: '0.1em',
     marginBlockStart: '0',
   },
-  languageContainer: {
-    marginTop: 'auto',
-    marginBottom: '6em',
-    textAlign: 'center',
-  },
-  buttomLang: {
+  yearButton: {
     color: '#fff',
     fontWeight: '900',
   },
@@ -70,52 +56,48 @@ export default function Search(props) {
 
   // TODO: i bottoni .active devono avere il color viola!
   return (
-    <div className={classes.homeContainer}>
-      <Bar lang={lang} />
-      <Container fixed>
-        <Typography className={classes.boxContainer} component="div">
-          <Box className={classes.textBox}>
-            <h5>HOME MOVIES DIGITAL ARCHIVE</h5>
-            <h1 className={classes.mainTitle}>Viaggio in Italia</h1>
-          </Box>
-          <Box>
-            <Grid
-              justify="space-between"
-              container 
-              alignItems="center"
-              >
-              <Grid item>
-                <Box component="div">
-                  <Button classes={{root: classes.buttomLang}} 
-                          className={ !qstring.decade ? 'active' : ''} 
-                          onClick={() => go2page('./')}
-                          >
-                    All
-                  </Button>
-                  {
-                    [20, 30, 40, 50, 60, 70, 80].map((e, i) => {
-                      return (
-                        <Button
-                          classes={{root: classes.buttomLang}}
-                          className={ Number(qstring.decade) === e ? 'active' : ''}
-                          key={i}
-                          onClick={() => go2page(`./?decade=${e}`)}>'{e}</Button>
-                          )})
-                    }
-                  </Box>
-              </Grid>
-
-              <Grid item className={classes.filterContainer}>
-                <FilterModal />
-              </Grid>
+    <InternalTmpl lang={lang}>
+      <Typography className={classes.boxContainer} component="div">
+        <Box className={classes.textBox}>
+          <h5>HOME MOVIES DIGITAL ARCHIVE</h5>
+          <h1 className={classes.mainTitle}>Viaggio in Italia</h1>
+        </Box>
+        <Box>
+          <Grid
+            justify="space-between"
+            container 
+            alignItems="center"
+            >
+            <Grid item>
+              <Box component="div">
+                <Button classes={{root: classes.yearButton}} 
+                        className={ !qstring.decade ? 'active' : ''} 
+                        onClick={() => go2page('./')}
+                        >
+                  <FormattedHTMLMessage id="app.search.all" />
+                </Button>
+                {
+                  [20, 30, 40, 50, 60, 70, 80].map((e, i) => {
+                    return (
+                      <Button
+                        classes={{root: classes.yearButton}}
+                        className={ Number(qstring.decade) === e ? 'active' : ''}
+                        key={i}
+                        onClick={() => go2page(`./?decade=${e}`)}>'{e}</Button>
+                        )})
+                  }
+                </Box>
             </Grid>
-            
-          </Box>
-        </Typography>
 
-        <Results qs={qstring} />
+            <Grid item className={classes.filterContainer}>
+              <FilterModal />
+            </Grid>
+          </Grid>
+          
+        </Box>
+      </Typography>
 
-      </Container>
-    </div>
+      <Results qs={qstring} />
+    </InternalTmpl>
   );
 }
