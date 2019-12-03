7b7705a7ef1aa80814241d18ef45b413b0805f93
bdus-dev <developer@bradypus.net>
2019-12-03 11:34:47 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.130465e67f3b1f85736e527297a0b339.js"
+  "/precache-manifest.ffdc98a67297aad9bbfdf1cb968efd8c.js"
 );
 
 self.addEventListener('message', (event) => {
