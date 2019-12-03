1d7b4ac65d8531dc56524e80edf878b1aa922511
bdus-dev <developer@bradypus.net>
2019-12-03 16:41:13 +0100

    Updates

---
+++ precache-manifest.130465e67f3b1f85736e527297a0b339.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad28ffa19928e751522ddf00ce5a8180",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b813b19b5a63860849aa",
+    "url": "/static/css/main.6e017a41.chunk.css"
+  },
+  {
+    "revision": "0390e7dd4eb3a742b0a9",
+    "url": "/static/js/2.c9206a86.chunk.js"
+  },
+  {
+    "revision": "b813b19b5a63860849aa",
+    "url": "/static/js/main.a14ed494.chunk.js"
+  },
+  {
+    "revision": "cf5b90b13ebfe68ec89c",
+    "url": "/static/js/runtime-main.d0916a98.js"
+  },
+  {
+    "revision": "69f0cdef6436f6a6bfe630ce45e0811a",
+    "url": "/static/media/home-background.69f0cdef.jpg"
+  },
+  {
+    "revision": "a7496dd4e869cf278e4a07927dea52b6",
+    "url": "/static/media/internal-background.a7496dd4.jpg"
+  },
+  {
+    "revision": "f8f6540de1be69ea9c4393fe8e1992b2",
+    "url": "/static/media/video-bg.f8f6540d.mp4"
+  }
+]);
