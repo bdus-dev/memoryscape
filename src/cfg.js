068142b8101dc824f44e27c68196355853044d82
bdus-dev <developer@bradypus.net>
2019-12-19 07:45:36 +0100

    configuration depends on window.location.href

--- src/cfg.js
+++ src/cfg.js
@@ -1,6 +1,22 @@
-export const app = 'er';
-export const col = '#7a1dcf';
-export const title = {
-    main: 'memoryscapes 01',
-    sub: 'lungo la via emilia'
+const variables = {
+    er: {
+        app: 'er',
+        col: '#7a1dcf',
+        title: {
+            main: 'memoryscapes 01',
+            sub: 'lungo la via emilia'
+        }
+    },
+
+    italia: {
+        app : 'italia',
+        col : '#7a1dcf',
+        title : {
+            main: 'memoryscapes 02',
+            sub: 'viaggio in italia'
+        }
+    }
 };
+
+const index = window.location.href.includes('ms-emilia-romagna.homemovies.it') ? 'er' : 'italia';
+export const {app, col, title} = variables[index];
