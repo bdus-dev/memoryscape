44d85fb7fb2a8a459450fba50b1212ec73b51ad9
bdus-dev <developer@bradypus.net>
2019-12-20 13:51:27 +0100

    Green for Italia

--- src/cfg.js
+++ src/cfg.js
@@ -10,13 +10,14 @@ const variables = {
 
     italia: {
         app : 'italia',
-        col : '#7a1dcf',
+        col : '#245344',
         title : {
             main: 'memoryscapes 02',
-            sub: 'viaggio in italia'
+            sub: 'cartoline italiane'
         }
     }
 };
 
 const index = window.location.href.includes('ms-emilia-romagna.homemovies.it') ? 'er' : 'italia';
+// export const {app, col, title} = variables['er'];
 export const {app, col, title} = variables[index];
